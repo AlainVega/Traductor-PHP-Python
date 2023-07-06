e7e213a7d4adfa9d57c9ea4c37a99bca3df1cdcb
Mathias Martinez <mathias.martinez018@gmail.com>
2023-07-06 16:39:50 -0400

    Modificar condicionales.php

--- traductor/ejemplos/condicionales.php
+++ traductor/ejemplos/condicionales.php
@@ -13,6 +13,19 @@ else {
     echo "Y esto!\n";
 }
 
+$x = 0;
+$y = 3;
+
+if ($x > $y) {
+    echo "x es mayor que y\n";
+}
+elseif ($x < $y) {
+    echo "y es mayor que x\n";
+}
+else {
+    echo "x e y son iguales\n";
+}
+
 if (1 > 1) {
     
 }
