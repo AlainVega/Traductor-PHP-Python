ae391c41016c87d90d3e785c61e66c2260855ff9
Alain Vega <alainjosevz@gmail.com>
2023-07-07 17:39:19 -0400

    Estabilizacion del merge

--- ejemplos/correctos/condicionales.php
+++ ejemplos/correctos/condicionales.php
@@ -28,12 +28,5 @@ else {
     echo "Esto esta en un else\n";
 }
 
-if (1 > 1) 
-    echo "Esto esta en un if\n";
-elseif (1 < 2) 
-    echo "Esto esta en un elseif\n";
-else {
-    echo "Esto esta en un else\n";
-}
 
 ?>
